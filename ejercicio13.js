16c8d565e102d85b7766e6f8d8e582aeb0646f7c
Sebastian <sbernal@unsa.edu.pe>
2025-10-23 21:24:50 -0500

    Ejercicio 13

---
+++ ejercicio13.js
@@ -0,0 +1,29 @@
+console.log("Carrito de compras");
+let totalParcial=0, totalFinal;
+while (true){
+    let continuar= prompt("¿Continuar?(y/n)");
+    if(continuar==="y"){
+        let precio=parseFloat(prompt("Ingresar precio"));
+        totalParcial+=precio;
+    }
+    else if(continuar==="n"){
+        break;
+    }
+    else{
+        console.log("Opcion no valida");
+    }
+}
+console.log("Total parcial a pagar: "+totalParcial);
+if(totalParcial>100){
+    console.log("Tiene un descuento del 10%");
+    totalFinal=totalParcial*0.9;
+}
+else if(totalParcial>=50){
+    console.log("Gana un cupón de 5%");
+    totalFinal=totalParcial*0.95;
+}
+else{
+    console.log("No aplica descuento");
+    totalFinal=totalParcial;
+}
+console.log("Total a pagar: "+totalFinal);
